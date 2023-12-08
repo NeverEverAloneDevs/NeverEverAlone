f7455aa4fb3a7f56a38d9132d6e3893640c4dd7e
Hui Zhao <huizhao1003@gmail.com>
2023-12-07 21:42:50 -0500

    added Outlet from react-router-dom to render components in children path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
 import logo_name from '../assets/logo_name.svg'
-
+import { Outlet } from "react-router-dom";
 
 export default function Navbar(){
     return (
+        // navbar: render all paths in the Router.jsx
         <nav>
         <img  src={logo_name} alt='logo and name of NeverEverAlone'/>
         <Link to="/">Home</Link>
@@ -11,6 +12,13 @@ export default function Navbar(){
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
         <Link to="/donate-now">Donate now</Link>
+        {/* import Outlet so all the components in children paths can render */}
+        <div>
+            <Outlet />
+        </div>
+
         </nav>
+
+        
     )
 }
