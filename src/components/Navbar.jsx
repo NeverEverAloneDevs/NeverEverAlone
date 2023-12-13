5967556595d9f32fb3cd847dddc86f02a86f50e4
Hui Zhao <huizhao1003@gmail.com>
2023-12-12 20:33:25 -0500

    navbar basic css completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,27 @@
 import { Link } from 'react-router-dom';
 import logo_name from '../assets/logo_name.svg'
 import { Outlet } from "react-router-dom";
-
 export default function Navbar(){
     return (
         // navbar: render all paths in the Router.jsx
+    <div>
         <nav>
         <img  src={logo_name} alt='logo and name of NeverEverAlone'/>
-        <Link to="/">Home</Link>
-        <Link to="/domestic-violence">Domestic Violence</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/donate-now">Donate now</Link>
+        <div className='nav-items'>
+        <Link className='nav-home'to="/">Home</Link>
+        <Link className='nav-rest-items'to="/domestic-violence">Domestic Violence</Link>
+        <Link className='nav-rest-items'to="/about">About</Link>
+        <Link className='nav-rest-items'to="/contact">Contact</Link>
+        <Link className='nav-rest-items'to="/donate-now">Donate now</Link>
+        </div>
+
         {/* import Outlet so all the components in children paths can render */}
         <div>
             <Outlet />
         </div>
 
         </nav>
-
+</div>
         
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import './App.css'
 import router from './Router';
 import App from './App';
 
