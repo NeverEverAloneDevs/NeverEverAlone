d0f3c30847df918e2c8a794b5e5959a321bbc412
NeverEverAlone <147873673+NeverEverAlone@users.noreply.github.com>
2024-01-11 03:18:42 +0000

    who we are is done! need add correct url to 'learn more' button

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,34 @@ export default function About() {
     return (
        <div>
 
-                <div>
-                <p>Who We Are</p>
+                <div className="who-we-are">
+                        <div className="who-we-are-images-contents">
+                                <div className="image-1-box">
+                                        <img className="image-1" src="./assets/About_WhoWeAre/image40.svg" />
+                                </div>
+                               <div className="image-2-box">
+                                        <img className="image-2" src="./assets/About_WhoWeAre/image39.svg" />
+                                </div>
+                                <div className="who-we-are-contents-box">
+                                        <h4  className="who-we-are-content-title">
+                                        Who We Are
+                                        </h4>
+                                        <p className="who-we-are-content-p">
+                                        Nevereveralone is a compassionate and dedicated organization comprised of individuals who are passionate about ending domestic violence and providing support to victims and survivors.
+                                        </p>
+                                        <p className="who-we-are-content-p">
+                                        We are a diverse team of volunteers, advocates, professionals, and survivors who share a common goal of creating a world free from domestic violence. Through our collective expertise, empathy, and commitment, we strive to make a positive impact on the lives of those affected by domestic violence, offering them the support, resources, and empowerment they need to rebuild their lives and thrive.
+                                        </p>
+                                        <div className="who-we-are-button-box">
+                                        <a className="who-we-are-button" href="www.google.com">Learn More </a>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+  <path d="M7.5 15L12.5 10L7.5 5" stroke="#147952" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+                                        </div>
+                                        
+                                </div>
+                        </div>
+                        
                 </div>
 
                 <div>
@@ -15,5 +41,6 @@ export default function About() {
                 <div>
                 <p>What we do</p>
                 </div>
-        </div>)
+        </div>
+        )
 }
