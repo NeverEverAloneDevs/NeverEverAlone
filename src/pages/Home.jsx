c814aadf44a7add25e59d92d409d301ab3654ea3
NeverEverAlone <147873673+NeverEverAlone@users.noreply.github.com>
2024-01-11 01:34:11 +0000

    remove svg file in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,11 @@ import About from "../components/About";
 import Contact from "../components/Contact";
 import DomesticViolence from "../components/DomesticViolence";
 import Footer from "../components/Footer";
-// import getHelp from "../assets/getHelp.svg"
 export default function Home() {
     return(
         <>   
         <div className="home-nav-box"> 
             <Navbar />      
-            {/* <img  src={getHelp} alt="getHelp svg for background"/> */}
         </div>
         <div>
             <About />
