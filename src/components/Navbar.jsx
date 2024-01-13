7f9ac29a25054e24479818cafd621acff3f1f2ee
NeverEverAlone <147873673+NeverEverAlone@users.noreply.github.com>
2024-01-13 03:50:29 +0000

    changed navbar css and added 'download now' button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,22 @@ import logo_name from '../assets/logo_name.svg'
 // import { Outlet } from "react-router-dom";
 export default function Navbar(){
     return (
-    
         // navbar: render all paths in the Router.jsx
-    <div className='back-ground'>
-        
-        <nav>
-        <img  src={logo_name} alt='logo and name of NeverEverAlone'/>
-        <div className='nav-items'>
-            <Link className='nav-home'to="/">Home</Link>
-            <Link className='nav-rest-items'to="/domestic-violence">Domestic Violence</Link>
-            <Link className='nav-rest-items'to="/about">About</Link>
-            <Link className='nav-rest-items'to="/contact">Contact</Link>
-            <Link className='nav-rest-items'to="/donate-now">Donate now</Link>
+        <div className='back-ground'>
+            
+            <nav>
+            <img  src={logo_name} alt='logo and name of NeverEverAlone'/>
+            <div className='nav-items'>
+                <Link className='nav-home'to="/">Home</Link>
+                <Link className='nav-rest-items'to="/domestic-violence">Domestic Violence</Link>
+                <Link className='nav-rest-items'to="/about">About</Link>
+                <Link className='nav-rest-items'to="/contact">Contact</Link>
+                <Link className='nav-rest-items'to="/donate-now">Donate now</Link>
+                <a className='download-now-button'>Download App</a>
+            </div>
+            </nav>
+            
         </div>
-        </nav>
-        
-</div>
-        
+            
     )
 }
