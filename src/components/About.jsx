ddb5546550d3c46d9b55846702140c9617886f46
NeverEverAlone <147873673+NeverEverAlone@users.noreply.github.com>
2024-01-13 03:25:51 +0000

    Added 'what is nea doing'

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@
 export default function About() {
     return (
        <div>
-
+                {/* who we are */}
                 <div className="who-we-are">
                         <div className="who-we-are-images-contents">
                                 <div className="image-1-box">
@@ -17,8 +17,8 @@ export default function About() {
                                         </h4>
                                         <p className="who-we-are-content-p">
                                         Nevereveralone is a compassionate and dedicated organization comprised of individuals who are passionate about ending domestic violence and providing support to victims and survivors.
-                                        </p>
-                                        <p className="who-we-are-content-p">
+                                        <br />
+                                        <br />
                                         We are a diverse team of volunteers, advocates, professionals, and survivors who share a common goal of creating a world free from domestic violence. Through our collective expertise, empathy, and commitment, we strive to make a positive impact on the lives of those affected by domestic violence, offering them the support, resources, and empowerment they need to rebuild their lives and thrive.
                                         </p>
                                         <div className="who-we-are-button-box">
@@ -33,14 +33,21 @@ export default function About() {
                         
                 </div>
 
-                <div>
-                <p>Who We Serve</p>
+                {/* what is NeverEverAlone doing/ what we do */}
+                <div className="what-we-do-section">
+                        <div className="what-we-do-left-box">
+                                <h4 className="what-we-do-title">What is NeverEverAlone Doing?</h4>
+                                <p className="what-we-do-title-content">
+                                Nevereveralone is working to Rebuild Lives and Restore Hope by developing an App for DV Victims & Survivors.
+                                </p>
+                        </div>
+                        <div className="what-we-do-right-box">
+                                <p className="what-we-do-right-content">
+                                Our app is dedicated to empowering domestic violence victims and survivors by providing them with the resources and support they need to take control of their lives. Our mission is to create a safe and confidential space where victims and survivors can find help and support to escape their abusive situations, heal from their experiences, and rebuild their lives. We strive to provide comprehensive resources that ensure victims and survivors have access to a network of support including legal assistance, counselling, safe shelter, and emergency services. Our app is designed to be user-friendly and accessible, with a focus on privacy, confidentiality, and security. We are committed to helping victims and survivors regain their sense of self-worth and independence, and to empowering them to live a life free from violence and abuse.
+                                </p>
+                        </div>
                 </div>
                 
-                
-                <div>
-                <p>What we do</p>
-                </div>
         </div>
         )
 }
