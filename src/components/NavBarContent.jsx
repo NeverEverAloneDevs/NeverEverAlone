8b6dbf95569b071c4df400abed0c273c5c19d9b9
Yen P <jinapham.0611@gmail.com>
2024-01-20 22:16:11 -0500

    apply tailwind to navbar, navbarContent

--- src/components/NavBarContent.jsx
+++ src/components/NavBarContent.jsx
@@ -1,14 +1,16 @@
 export default function NavBarContent() {
 
     return (
-        <div className="navbar-content-box">
-            <h5 className="navbar-content">
-            “Domestic violence is a human rights violation, and it’s our duty to ensure that every person has the right to live free from fear and abuse.”
-            </h5>
-            <p className="navbar-content-author">
-            -Emma Watson
-            </p>
-            <button className="get-help">Get Help</button>
+        <div className=" font-montserrat bg-cover bg-center bg-[url('./assets/getHelp.svg')] h-screen md:h-auto bg-image-mobile md:bg-image-desktop" >
+            <div className="flex flex-col start-left justify-center py-60 inset-0 pl-20 pr-10 lg:pr-60">
+                <h5 className="font-montserrat text-gray-300 text-2xl md:text-3xl py-3" style={{ lineHeight: '1.5em' }}>
+                “Domestic violence is a human rights violation, and it’s our duty to ensure that every person has the right to live free from fear and abuse.”
+                </h5>
+                <p className=" font-montserrat text-white text-lg md-xl py-3 ">
+                -Emma Watson
+                </p>
+                <button className="font-montserrat bg-green-600 rounded-full px-1 py-3 mt-4 w-32">Get Help</button>
+            </div>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat'],
+        
+    }
+    },
   },
   plugins: [],
 }
