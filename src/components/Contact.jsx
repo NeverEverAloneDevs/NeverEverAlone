8b136ae82cc50dfa4dd9cc56405b2a420910d84a
NeverEverAlone <147873673+NeverEverAlone@users.noreply.github.com>
2024-01-22 03:37:23 +0000

    contact section app store photos QR code done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,17 @@
 export default function Contact() {
     return (
         
-        <p>contact us now</p>
+       <div className="appdownload-section">
+        <div className="appdownload-left-box"/>
+        <div className="appdownload-right-box">
+            <p className="download-app">Download and register on the app to get help</p>
+            <div className="appstore-box">
+                <div className="apple-store" /> 
+                <div className="googleplay-store" />
+            </div>
+            <div className="qrcode" />
+        </div>
+      </div>
        
     )
 }
