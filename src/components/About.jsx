26d758a65f7cff486428133a5b952dac9f646ac4
Yen P <jinapham.0611@gmail.com>
2024-02-07 21:29:51 -0500

    add git gb for deploy with github

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -37,7 +37,7 @@ export default function About() {
 			</div>
 
 			{/* what is NeverEverAlone doing/ what we do */}
-			<div className="font-montserrat p-10 flex flex-col md:flex-row gap-10">
+			<div className="font-montserrat p-10 flex flex-col md:flex-row gap-10 text-lg">
 				<div className="flex flex-col gap-5 ">
 					<h4 className="md:text-5xl text-3xl font-bold underline underline-offset-8 decoration-green-600 w-full ">What we do?</h4>
 					<p className="mt-5">
@@ -49,17 +49,17 @@ export default function About() {
 				<div className="flex flex-col md:flex-row gap-5 w-full">
 					<div className=''>
 						<h5 className='font-bold'>Mobile App</h5>
-						<p>Our app is dedicated to empowering domestic violence victims and survivors by providing them with the resources and support they need to take control of their lives. </p>
+						<p className='mt-5'>Our app is dedicated to empowering domestic violence victims and survivors by providing them with the resources and support they need to take control of their lives. </p>
 				
 						<h5 className='font-bold mt-8'>Mission</h5>
-						<p>Our mission is to create a safe and confidential space where victims and survivors can find help and support to escape their abusive situations, heal from their experiences, and rebuild their lives. </p>
+						<p className='mt-5'>Our mission is to create a safe and confidential space where victims and survivors can find help and support to escape their abusive situations, heal from their experiences, and rebuild their lives. </p>
 					</div>
 					<div className=''>
 						<h5 className='font-bold'>Provide</h5>
-						<p>We strive to provide comprehensive resources that ensure victims and survivors have access to a network of support including legal assistance, counselling, safe shelter, and emergency services. </p>
+						<p className='mt-5'>We strive to provide comprehensive resources that ensure victims and survivors have access to a network of support including legal assistance, counselling, safe shelter, and emergency services. </p>
 					
 						<h5 className='font-bold mt-8'>Commitment</h5>
-						<p>We are committed to helping victims and survivors regain their sense of self-worth and independence, and to empowering them to live a life free from violence and abuse.</p>
+						<p className='mt-5'>We are committed to helping victims and survivors regain their sense of self-worth and independence, and to empowering them to live a life free from violence and abuse.</p>
 					</div>
 				</div>
 			</div>
--- src/components/DomesticViolence.jsx
+++ src/components/DomesticViolence.jsx
@@ -1,24 +1,23 @@
-
+import dv from '../assets/DomesticViolence.svg';
 export default function DomesticViolence() {
     return (
-        <div className="DV-section">
-            <div className="DV-left-box">
-                <h4 className="DV-title">Understanding Domestic Violence</h4>
-                <p className="DV-content">
+        <div className="flex md:flex-row flex-col bg-green-100  gap-10 text-lg">
+            <div className="w-full p-10">
+                <h4 className="text-5xl font-bold underline underline-offset-8  decoration-green-600">Understanding Domestic Violence</h4>
+                <p className="mt-8">
                 Domestic violence is a pattern of behaviour in which one person in a relationship tries to control and dominate the other person through physical, emotional, or sexual abuse. It can occur in any type of relationship, including those between intimate partners, family members, and roommates.
                 <br />
                 <br />
                 Victims of domestic violence often experience fear, anxiety, and isolation. They may feel trapped in their situation and be unable to leave due to financial, emotional, or physical barriers. Victims may also feel ashamed or embarrassed about their situation and struggle to seek help. This is why the app we are creating has the option to stay anonymous and get help safely from their home or wherever they are comfortable without being Judged and associating shame with it!
                 </p>
-                <div className="DV-button-box">
-            <a className="DV-button" href="www.google.com">Learn More </a>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-  <path d="M7.5 15L12.5 10L7.5 5" stroke="#147952" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
+                <div className='mt-5 flex items-center'>
+                    <a className="who-we-are-button" href="www.google.com">Learn More </a>	
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                    <path d="M7.5 15L12.5 10L7.5 5" stroke="#147952" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
+				</div>
             </div>
-            </div>
-            <div className="DV-image-box">
-            <div className="DV-image"></div>
+            <div className="w-full right-0 ">
+            <img className="" src={dv} alt='img40'></img>
             </div>
             
         </div>
